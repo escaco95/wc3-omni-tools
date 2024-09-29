a0e301b8b797e2cea7b26415fffd5888127e1c24
Choi <escaco95@naver.com>
2024-09-30 06:41:42 +0900

    🔧 WC3OmniTool > Templating modern styles
    - templated modern simple button

---
+++ WC3OmniTool/ModernStyles/ModernSimpleButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WC3OmniTool.ModernStyles
+{
+    public class ModernSimpleButton : Button
+    {
+        // HotBackground Property (= Hover & Pressed)
+        public static readonly DependencyProperty HotBackgroundProperty =
+            DependencyProperty.Register("HotBackground", typeof(Brush), typeof(ModernSimpleButton), new PropertyMetadata(Brushes.Transparent));
+
+        public Brush HotBackground
+        {
+            get => (Brush)GetValue(HotBackgroundProperty);
+            set => SetValue(HotBackgroundProperty, value);
+        }
+
+        // HotForeground Property (= Hover & Pressed)
+        public static readonly DependencyProperty HotForegroundProperty =
+            DependencyProperty.Register("HotForeground", typeof(Brush), typeof(ModernSimpleButton), new PropertyMetadata(Brushes.Transparent));
+
+        public Brush HotForeground
+        {
+            get => (Brush)GetValue(HotForegroundProperty);
+            set => SetValue(HotForegroundProperty, value);
+        }
+    }
+}
